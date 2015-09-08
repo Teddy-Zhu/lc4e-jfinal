94eafe8d8e7731fcf5f96886f51f6f74d9fc1a8f
teddy <teddy.zhu@lc4e.com>
2015-09-08 09:29:52 +0800

    fix bug about beetltool

---
+++ src/main/java/com/teddy/jfinal/plugin/beetl/BeetlTool.java
@@ -0,0 +1,10 @@
+package com.teddy.jfinal.plugin.beetl;
+
+/**
+ * Created by teddy on 2015/9/5.
+ */
+public class BeetlTool {
+    public Long getTime() {
+        return System.currentTimeMillis();
+    }
+}
